68699f7aec43e3287c479b63bc068b30f986341d
Martin Herrmann <martin@famherrmann.de>
2022-03-07 21:34:05 +0100

    Remove old code

--- old/test/all_tests.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-Dir.glob("#{File.dirname(__FILE__)}/*_test.rb").each do |entry|
-	require_relative "#{File.basename(entry)}"
-end
-
-# at_axit, the AutoRunner will be invoked
-
